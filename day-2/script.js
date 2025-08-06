1e05448e16f43f2f6ee2bea9791472b90804159d
sonaliAy <sonalijaiswals2005@gmail.com>
2025-08-06 13:36:37 +0530

    day-2

--- day-2/script.js
+++ day-2/script.js
@@ -1,7 +1,16 @@
 //ARITHMETIC OPERATOR
-let a = 5;
+/*let a = 5;
 let b = 2;
 console.log("a+b=",a+b);
 console.log("a-b=",a-b);
 console.log("a*b=",a*b);
+console.log("a/b=",a/b);*/
+
+//EXAMPLE
+let a = 3;
+let b = 4;
+console.log("a+b=",a+b);
+console.log("a-b=",a-b);
+console.log("a*b=",a*b);
 console.log("a/b=",a/b);
+
