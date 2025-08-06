83eaa3f700ac305043fdf8ababf760a18778ec77
sonaliAy <sonalijaiswals2005@gmail.com>
2025-08-07 01:30:53 +0530

    day-2

--- day-2/script.js
+++ day-2/script.js
@@ -124,13 +124,34 @@ console.log(result);*/
 //LET'S PRACTICE
 //1.MULTIPLE 5 OR NOT
 
-let num = ("enter a number:");
+/*let num = ("enter a number:");
 if(num % 5===0)
 {
     console.log(num," is a multiple of 5");
 }
 else{
     console.log(num,"is not a multiple of 5");
+}*/
+
+//GRADES
+let score = 75;
+let grade;
+if(score>=90 && score<=100)
+{
+    grade = "A";
 }
+else if(score>=70 && score<=89){
+    grade = "B";
+}
+else if(score>=60 && score<=69){
+    grade = "C";
+}
+else if(score>=50 && score<=59){
+    grade = "D";
+}
+else if(score>=0 && score<=49){
+    grade = "F";
+}
+console.log("according to your scores,your grade was:",grade);
 
 
