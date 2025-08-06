5e43b941ffcb948fa94869d24b2a38199cb11e60
sonaliAy <sonalijaiswals2005@gmail.com>
2025-08-06 23:02:01 +0530

    day-2

--- day-2/script.js
+++ day-2/script.js
@@ -8,16 +8,29 @@ console.log("a/b=",a/b);*/
 
 
 //EXAMPLE
-let a = 3;
+/*let a = 3;
 let b = 4;
 console.log("a+b=",a+b);
 console.log("a-b=",a-b);
 console.log("a*b=",a*b);
 console.log("a/b=",a/b);
 console.log("a**b=",a**b);
-console.log("a%b=",a%b);
+console.log("a%b=",a%b);*/
 
 
+//ASSIGNMENT OPERATOR
+let a = 5;
+let b = 2;
+a += 4;  //a=a+4
+console.log("a=",a);  
+a -= 4 ;  //a=a-4
+console.log("a=",a);
+a *= 4;  //a=a*4
+console.log("a=",a);
+a %= 4; //a=a%4
+console.log("a=",a);
+a **= 4;  //a=a**4
+console.log("a=",a);
 
 
 
