b24daab0b86d06a8206f183491cc378ec021bde2
sonaliAy <sonalijaiswals2005@gmail.com>
2025-08-07 22:07:06 +0530

    day-3

---
+++ day-3/script.js
@@ -0,0 +1,12 @@
+//LOOP
+/*for(let i=1;i<=5;i++){
+    console.log("good morning everyone");
+}*/
+
+// CALCULATE SUM OF 1 TO 5
+let sum = 0;
+for(let i=1;i<=5;i++){
+    sum = sum+i;
+}
+console.log("sum=",sum);
+console.log("loop has end ");
