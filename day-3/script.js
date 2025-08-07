6a18b8e61314b861524e8b2bf73d6aa0f6ffb90d
sonaliAy <sonalijaiswals2005@gmail.com>
2025-08-07 22:53:30 +0530

    day-3

--- day-3/script.js
+++ day-3/script.js
@@ -33,7 +33,16 @@ do{
 }while(i<=5);*/
 
 //FOR-OF LOOP
-let str = "hey everyone";
+/*let str = "hey everyone";
 for(let i of str){
     console.log("i=",i);
+}*/
+
+//EXAMPLE
+let str = "javascript";
+let size = 0;
+for(let i of str){
+    console.log("i=",i);
+    size++;
 }
+console.log("string size=",size);
