e8cc740d28f64abde31d249c94c2ccdb9855ac88
sonaliAy <sonalijaiswals2005@gmail.com>
2025-08-22 21:04:56 +0530

    day-4

--- day-4/script.js
+++ day-4/script.js
@@ -1,4 +1,10 @@
 //ARRAYS
-let marks = [97,82,75,64,36];
+/*let marks = [97,82,75,64,36];
 console.log(marks);
-console.log(marks.length);
+console.log(marks.length);  //property*/
+
+//EXAMPLE
+let heroes = ["spiderman","ironman","thor","hulk"];
+console.log(heroes);
+console.log(heroes.length);
+typeof heroes;
