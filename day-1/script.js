1371b8ee7b3062ef41d11998357c3cb280ecb7c0
sonaliAy <sonalijaiswals2005@gmail.com>
2025-11-18 11:29:27 +0530

    day-3

--- day-1/script.js
+++ day-1/script.js
@@ -1,7 +1,15 @@
-const product = {
+/*const product = {
     title:"ball pen",
     rating:4,
     offer:5,
     price:270,
 };
-console.log(product);
+console.log(product);*/
+
+var a = 10;
+var b = 20;
+if(a===b){
+    console.log("true");}
+else{
+    console.log("false");
+}
--- day-4/script.js
+++ day-4/script.js
@@ -26,9 +26,46 @@ console.log (marks[1]);*/
         console.log(hero);
     }*/
 
-        let cities = ["delhi","lucknow","gorakhpur","pune","bihar"];
+        /*let cities = ["delhi","lucknow","gorakhpur","pune","bihar"];
         for(let city of cities){
            // console.log(city);
            console.log(city.toUpperCase());
-        }
+        }*/
+
+        //  let name = ["aman","ram","sohan","sona"];
+        // for(let name of name){
+        //    // console.log(name);
+        //    console.log(name.toUpperCase());
+        // }
+
+           //PRACTICE QUESTION 
+            // let marks = [85,97,44,37,60];
+            // let sum = 0;
+            // for(let val of marks){
+            //     sum+=val;
+
+            // }
+            // let avg = sum / marks.length;
+            // console.log('avg marks of the class = ${avg}');
     
+
+
+            // let items =[250,645,300,900,50];
+            // let idx= 0;
+            // for(let val of items){
+            //     console.log(`value at index ${idx} = ${val}`);
+            //     let offer =val/10;
+            //     items[idx] = items[idx]-offer;
+            //     console.log(`value after offer =${items[idx]}`)
+            //     idx++;
+            // }
+            // let items =[250,645,300,900,50];
+            // for(let i=0;i<items.length;i++){
+            //       let offer = items[i]/10;
+            //       items[i]-=offer;
+            // }
+            // console.log(items);
+
+            //ARRAYS IN JS  
+            
+            
