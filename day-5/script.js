0336d3318d93cbe07a3b9687e6d9e3d356d9617d
sonaliAy <sonalijaiswals2005@gmail.com>
2025-11-19 23:19:08 +0530

    day-5

---
+++ day-5/script.js
@@ -0,0 +1,48 @@
+//function
+// function myFunction(){
+//     console.log("apna college");
+//     console.log("we are learning js");
+// }
+
+// myFunction();
+// myFunction();
+// myFunction();
+
+// function myFunction(msg){
+//     //parameter->input
+//     console.log(msg);
+// }
+// myFunction("i love js")//argument
+
+
+//fuction ->2 number ,sum
+// function sum(a,b){
+//     console.log(a+b);
+// }
+// sum(1,3);
+
+// function sum(x,y){
+//     s=x+y;
+//     return s;
+// }
+// let val=sum(3,4);
+// console.log(val);
+
+//sum function
+function sum(a,b){
+    return a+b;
+}
+const arrowsum =(a,b)=>{
+    console.log(a+b);
+}
+
+
+//multiplication
+function mul(a,b){
+    return a*b;
+}
+
+
+arrowsum(a,b) =>{
+    console.log(a=b);
+}
